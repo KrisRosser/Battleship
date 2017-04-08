2e80ba5f00a505cac8120dad97ddf0fd962cbf77
tcotneyjsu <tcotney96@gmail.com>
2017-04-08 14:47:30 -0500

    Added a file
    Inside is the description for the purpose of this file.

---
+++ Battleship.java
@@ -0,0 +1,4 @@
+/* I am thnking that this class will handle the GUI much like minesweeper. I also believe that we should use
+the enable option should be used to distinguish between the two players. Maybe add a player_enable and AI_enable. I also think this class
+should house the code for the AI as well. I am also kinda thinking about using the observable even though I really do not want to.*/
+public class Battleship{}
