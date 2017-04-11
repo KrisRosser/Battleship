6342a1e9ad15694ba59049266c8b6669ba40fc91
msweatman <msweatman@stu.jsu.edu>
2017-04-10 19:17:50 -0500

    added icons

--- Battleship.java
+++ Battleship.java
@@ -1,4 +1,13 @@
 /* I am thnking that this class will handle the GUI much like minesweeper. I also believe that we should use
 the enable option should be used to distinguish between the two players. Maybe add a player_enable and AI_enable. I also think this class
 should house the code for the AI as well. I am also kinda thinking about using the observable even though I really do not want to.*/
-public class Battleship{}
+public class Battleship extends JFrame implements ActionListener, Observable {
+	
+	private final ImageIcon CARRIER_ICON = new ImageIcon(getClass().getClassLoader().getResource("carrier.png"));
+    private final ImageIcon BATTLESHIP_ICON = new ImageIcon(getClass().getClassLoader().getResource("battleship.png"));
+	private final ImageIcon CRUISER_ICON = new ImageIcon(getClass().getClassLoader().getResource("cruiser.png"));
+    private final ImageIcon SUBMARINE_ICON = new ImageIcon(getClass().getClassLoader().getResource("submarine.png"));
+	private final ImageIcon DESTROYER_ICON = new ImageIcon(getClass().getClassLoader().getResource("destroyer.png"));
+	
+	private Grid grid;
+}
