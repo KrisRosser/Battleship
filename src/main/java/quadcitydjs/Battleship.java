865c58da4b684bcfe2f31779ddd670ac0ea2f5ba
Kristofer Rosser <bushikris@gmail.com>
2017-04-11 20:41:26 -0500

    commented out battleship so I could compile

--- src/main/java/quadcitydjs/Battleship.java
+++ src/main/java/quadcitydjs/Battleship.java
@@ -4,7 +4,7 @@ import java.util.*;
 /* I am thnking that this class will handle the GUI much like minesweeper. I also believe that we should use
 the enable option should be used to distinguish between the two players. Maybe add a player_enable and AI_enable. I also think this class
 should house the code for the AI as well. I am also kinda thinking about using the observable even though I really do not want to.*/
-public class Battleship extends JFrame implements ActionListener, Observable {
+/*public class Battleship extends JFrame implements ActionListener, Observable {
 	
 	private final ImageIcon CARRIER_ICON = new ImageIcon(getClass().getClassLoader().getResource("carrier.png"));
     private final ImageIcon BATTLESHIP_ICON = new ImageIcon(getClass().getClassLoader().getResource("battleship.png"));
@@ -50,4 +50,4 @@ public class Battleship extends JFrame implements ActionListener, Observable {
 		
 	}
 	
-}
+}*/
