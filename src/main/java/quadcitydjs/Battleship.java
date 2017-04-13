228a920ba2a68b3fdd158e2a64074ae1cd7dc8be
msweatman <msweatman@stu.jsu.edu>
2017-04-13 12:20:36 -0500

    added main and debugged

--- src/main/java/quadcitydjs/Battleship.java
+++ src/main/java/quadcitydjs/Battleship.java
@@ -1,10 +1,15 @@
 package quadcitydjs;
+
+import java.lang.*;
+import javax.swing.*;
 import java.util.*;
+import java.awt.*;
+import java.awt.event.*;
 
 /* I am thnking that this class will handle the GUI much like minesweeper. I also believe that we should use
 the enable option should be used to distinguish between the two players. Maybe add a player_enable and AI_enable. I also think this class
 should house the code for the AI as well. I am also kinda thinking about using the observable even though I really do not want to.*/
-/*public class Battleship extends JFrame implements ActionListener, Observable {
+/* public class Battleship extends JFrame implements ActionListener, MouseListener {
 	
 	private final ImageIcon CARRIER_ICON = new ImageIcon(getClass().getClassLoader().getResource("carrier.png"));
     private final ImageIcon BATTLESHIP_ICON = new ImageIcon(getClass().getClassLoader().getResource("battleship.png"));
@@ -24,19 +29,19 @@ should house the code for the AI as well. I am also kinda thinking about using t
 		this(10,10);
 	}
 	
-	public Battleship() {
+	public Battleship(int height, int width) {
 		gridAI = new Grid(height, width, true);
-		gridAI.addObserver(this);
+		//gridAI.addObserver(this);
 		JPanel gridAIPanel = new JPanel();
 		gridAIPanel.setLayout(new GridLayout(height, width));
 		tile = new JLabel[height][width];
 		for(int i = 0; i < height; i++) {
 			for(int j = 0; j < width; j++) {
 				AI_tile[i][j] = new JLabel();
-				AI_tile[i][j] = setName("cell:" + i + ":" + j);
+				AI_tile[i][j].setName("cell:" + i + ":" + j);
 				AI_tile[i][j].setPreferredSize(new Dimension(50,50));
 				AI_tile[i][j].setHorizontalAlignment(0);
-				AI_tile[i][j].setBorder(Borderfactory.createRaisedBevelBored());
+				AI_tile[i][j].setBorder(BorderFactory.createRaisedBevelBorder());
 				add(tile[i][j]);
 				tile[i][j].addMouseListener(this);
 				gridAIPanel.add(tile[i][j]);
@@ -50,4 +55,5 @@ should house the code for the AI as well. I am also kinda thinking about using t
 		
 	}
 	
-}*/
+}
+*/
---
+++ src/main/java/quadcitydjs/Main.java
@@ -0,0 +1,9 @@
+package quadcitydjs;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+	}
+	
+}
