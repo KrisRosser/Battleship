89ff8420352c76c42d17e323cb27001bc2b0011e
msweatman <msweatman@stu.jsu.edu>
2017-04-18 18:28:39 -0500

    added main
    added main

--- src/main/java/quadcitydjs/Main.java
+++ src/main/java/quadcitydjs/Main.java
@@ -1,16 +1,17 @@
 package quadcitydjs;
 
+import javax.swing.*;
+
 public class Main {
 
 	public static void main(String[] args) {
 		Battleship board = null;
         if(args.length == 0) {
-            board = new Minesweeper();
+            board = new Battleship();
         }
-        else if(args.length == 3) {
-            board = new Minesweeper(Integer.parseInt(args[0]),
-                                    Integer.parseInt(args[1]),
-                                    Integer.parseInt(args[2]));
+        else if(args.length == 2) {
+            board = new Battleship(Integer.parseInt(args[0]),
+                                    Integer.parseInt(args[1]));
         }
         if(board == null) {
             System.out.println("You must either specify all of width/height/mines or none (default 8/8/10).");
